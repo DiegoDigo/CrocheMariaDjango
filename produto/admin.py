0bb5cc04546cd82a77b5e66b140ebf36a4540ca6
Diego <di3g0d0ming05@gmail.com>
2016-12-07 17:00:25 -0200

    Models

--- produto/admin.py
+++ produto/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Produto, Preco, Imagem
+from .models import Produto, Preco, Imagem, Tipo
+
+
+class AdminImagem(admin.ModelAdmin):
+    list_display = ['tipo', 'imagen']
+
+
+class AdminProduto(admin.ModelAdmin):
+    list_display = ['tipo', 'imagen']
+
+
+class AdminPreco(admin.ModelAdmin):
+    list_display = ['precoUnitario', 'precoPacote']
+
+
+class AdminTipo(admin.ModelAdmin):
+    list_display = ['tipo']
 
 admin.site.register(Preco)
 admin.site.register(Produto)
-admin.site.register(Imagem)
+admin.site.register(Imagem, AdminImagem)
+admin.site.register(Tipo, AdminTipo)
