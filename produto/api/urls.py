19c24bfe8b21ab671a767712ae16a988670cc736
Diego <di3g0d0ming05@gmail.com>
2016-12-09 11:30:25 -0200

    adicao swagger

--- produto/api/urls.py
+++ produto/api/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from . import views
+from rest_framework_swagger.views import get_swagger_view
+
+shema_view = get_swagger_view(title="Crochê Maria")
 
 urlpatterns = [
     url(r'^produtos/$', views.ListarProdutos.as_view()),
     url(r'^produto/(?P<pk>[0-9]+)/$', views.DetalheProduto.as_view()),
     url(r'^categorias/$', views.ListarCategorias.as_view()),
     url(r'^produto/categoria/(?P<categoria>[0-9]+)/$', views.ListarProdutoPorCategoria.as_view()),
+    url(r'^$', shema_view)
 ]
