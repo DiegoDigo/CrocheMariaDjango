71aa19e7b46eea24a377de8b9dfb52b3ee7ce2b9
Diego <di3g0d0ming05@gmail.com>
2016-12-09 13:49:29 -0200

    Serializer

---
+++ venda/api/serializers.py
@@ -0,0 +1,8 @@
+# from rest_framework import serializers
+# from cliente.models import ClienteModel , EnderecoModel
+#
+#
+# class SalvarCliente(serializers.ModelSerializer):
+#     class Meta:
+#         model = ClienteModel
+#         fields = ['']
